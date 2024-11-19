d652bcc3176cdfe33d794da0f7ba819a8172cf6e
Kate <100698139+k8sansom@users.noreply.github.com>
2024-11-19 19:44:57 +0100

    Update main.cpp

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,53 +1,30 @@
 #include "PmergeMe.hpp"
 #include <iomanip>
 
-template <typename Container, typename Pairs>
-void printMerge(PmergeMe<Container, Pairs> & merge)
+int main(int ac, char **av)
 {
-	Container	before = merge.getInput();
-
-	std::cout << "Before:\t\t";
-	for (typename Container::iterator it = before.begin(); it != before.end(); ++it)
+	if (ac < 2)
 	{
-		std::cout << std::fixed << *it << " ";
-	}
-	std::cout << std::endl;
-
-	Container	after = merge.getSorted();
-	std::cout << "After:\t\t";
-	for (typename Container::iterator it = after.begin(); it != after.end(); ++it)
-	{
-		std::cout << std::fixed << *it << " ";
-	}
-	std::cout << std::endl;
-}
-
-int main(int argc, char *argv[])
-{
-	if (argc < 2)
-	{
-		std::cerr << "Format: ./PmergeMe <int> <int> ... " << std::endl;
+		std::cerr << "Error. Usage: ./PmergeMe <positive integers>" << std::endl;
 		return 1;
 	}
 
+	std::vector<int> unsortedVec;
+	for (int i = 1; i < ac; i++)
 	{
-		std::vector<int> unsortedVec;
-		for (int i = 1; i < argc; i++)
+		int nbr = std::atoi(argv[i]);
+		if (nbr < 0)
 		{
-			int nbr = std::atoi(argv[i]);
-			if (nbr < 0)
-			{
-				std::cerr << "Error" << std::endl;
-				return 1;
-			}
-			unsortedVec.push_back(nbr);
+			std::cerr << "Error" << std::endl;
+			return 1;
 		}
-
-		PmergeMe<std::vector<int>, std::vector<Ints> > merge;
-		merge.startTime();
-		merge.computeInput(unsortedVec);
-		merge.endTime();
-		printMerge(merge);
+		unsortedVec.push_back(nbr);
+	}
+	PmergeMe<std::vector<int>, std::vector<Ints>> mergedVec;
+	mergedVec.startTime();
+	mergedVec.computeInput(unsortedVec);
+	mergedVec.endTime();
+	mergedVec.printMerge(mergedVec);
 		std::cout << "Time to process a range of " << merge.getSize() << " elements with std::vector : ";
 		std::cout << std::fixed << std::setprecision(1) << merge.getTime() << " us" << std::endl;
 	}
