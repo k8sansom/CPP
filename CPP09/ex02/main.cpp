b4dd3bf61c33f672ec5e039e4d0a6ebc30fb8570
Kate <100698139+k8sansom@users.noreply.github.com>
2024-11-19 20:28:07 +0100

    Update main.cpp

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,53 +1,39 @@
 #include "PmergeMe.hpp"
-#include <iomanip>
+#include <cstdlib>
+#include <iomanip> 
 
-int main(int ac, char **av)
-{
-	if (ac < 2)
-	{
+int main(int ac, char **av) {
+	if (ac < 2) {
 		std::cerr << "Error. Usage: ./PmergeMe <positive integers>" << std::endl;
 		return 1;
 	}
 
-	std::vector<int> unsortedVec;
-	for (int i = 1; i < ac; i++)
-	{
-		int nbr = std::atoi(argv[i]);
-		if (nbr < 0)
-		{
-			std::cerr << "Error" << std::endl;
-			return 1;
-		}
-		unsortedVec.push_back(nbr);
-	}
+	// Parse input and validate
+    	std::vector<int> inputValues;
+    	for (int i = 1; i < ac; ++i) {
+        	int nbr = std::atoi(av[i]);
+        	if (nbr < 0) {
+            		std::cerr << "Error: All numbers must be positive integers." << std::endl;
+            		return 1;
+        	}
+        	inputValues.push_back(nbr);
+   	}
+	//sort and print for vector
 	PmergeMe<std::vector<int>, std::vector<Ints>> mergedVec;
 	mergedVec.startTime();
-	mergedVec.computeInput(unsortedVec);
+	mergedVec.computeInput(inputValues);
 	mergedVec.endTime();
-	mergedVec.printMerge(mergedVec);
-		std::cout << "Time to process a range of " << merge.getSize() << " elements with std::vector : ";
-		std::cout << std::fixed << std::setprecision(1) << merge.getTime() << " us" << std::endl;
-	}
+	mergedVec.printMerged();
+	std::cout << "Time to process a range of " << mergedVec.getSize() << " elements with std::vector : ";
+	std::cout << std::fixed << std::setprecision(1) << mergedVec.getTime() << " us" << std::endl;
 
-	{
-		std::list<int> unsortedLst;
-		for (int i = 1; i < argc; i++)
-		{
-			int nbr = std::atoi(argv[i]);
-			if (nbr < 0)
-			{
-				std::cerr << "Error" << std::endl;
-				return 1;
-			}
-			unsortedLst.push_back(nbr);
-		}
-
-		PmergeMe<std::list<int>, std::list<Ints> > merge;
-		merge.startTime();
-		merge.computeInput(unsortedLst);
-		merge.endTime();
-		std::cout << "Time to process a range of " << merge.getSize() << " elements with std::list : ";
-		std::cout << std::fixed << std::setprecision(1) << merge.getTime() << " us" << std::endl;
-	}
+	//sort and print for list
+	std::list<int> inputList(inputValues.begin(), inputValues.end());
+	PmergeMe<std::list<int>, std::list<Ints> > mergedList;
+	mergedList.startTime();
+	mergedList.computeInput(inputList);
+	mergedList.endTime();
+	std::cout << "Time to process a range of " << mergedList.getSize() << " elements with std::list : ";
+	std::cout << std::fixed << std::setprecision(1) << mergedList.getTime() << " us" << std::endl;
 	return 0;
 }
